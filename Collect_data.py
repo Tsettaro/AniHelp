ada0899696bd70909a329081f03e77406f4eaa95
Artur <kamalovantoshka2018@gmail.com>
2021-07-15 17:08:09 +0300

    Add category "Значки" from Discomir

--- Collect_data.py
+++ Collect_data.py
@@ -73,6 +73,10 @@ def get_n(soup, id, shop):
         namae = [x for ind,x in enumerate(namae) if ind+1 not in lim]
         lim.clear()
         return namae
+    elif id == 'znachok' and shop == 'discomir':
+        for i in range(len(namae)):
+            namae[i] = namae[i][:-5]
+        return namae
 
 def get_p(soup, id, shop):
     if shop == 'discomir':
@@ -176,84 +180,100 @@ def get_p(soup, id, shop):
         pri = [x for ind, x in enumerate(pri) if ind + 1 not in lim_pr]
         lim_pr.clear()
         return pri
+    elif id == 'znachok' and shop == 'discomir':
+        return pri
 
+df = pd.read_excel('output.xlsx')
+df.pop('Unnamed: 0')
 namae = []
 price = []
-# Diskomir
-url = 'https://diskomir.ru/catalog/manga/manga/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8'
+url = "https://diskomir.ru/catalog/znachki/nabory_znachkov/?from_movie=bananovaya_ryba_banana_fish_rybka_bananka&from_movie=voleybol_haikyu_haikyuu_&from_movie=doktor_stoun_dr_stone_doctor_stone&from_movie=drakon_gornichnaya_kobayashi_kobayashi_san_chi_no_maid_dragon_miss_kobayashi_s_dragon_maid&from_movie=durochka_aho_girl_ahogaru_clueless_girl"
 page = requests.get(url)
 soup = BeautifulSoup(page.text, "html.parser")
-namae = namae + get_n(soup, 'manga', 'discomir')
-price = price + get_p(soup, 'manga', 'discomir')
-for k in range(2, 13):
-    url = 'https://diskomir.ru/catalog/manga/manga/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8&PAGEN_1=' + str(k)
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, "html.parser")
-    namae = namae + get_n(soup, 'manga', 'discomir')
-    price = price + get_p(soup, 'manga', 'discomir')
-del price[96]
-del namae[96]
-
-df = pd.DataFrame(columns=['name', 'price', 'shop', 'category'])
-for i in range(len(namae)):
-    if price[i].isdigit() == True:
-        _df = pd.DataFrame([[namae[i], int(price[i]), 'Discomir', 'Manga']], columns=['name', 'price', 'shop', 'category'])
-        df = df.append(_df, ignore_index=True)
-namae.clear()
-price.clear()
-url = 'https://diskomir.ru/catalog/animefigurki/malenkie_kitayskie_figurki/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-namae = namae + get_n(soup, 'figure', 'discomir')
-price = price + get_p(soup, 'figure', 'discomir')
-url = 'https://diskomir.ru/catalog/animefigurki/malenkie_kitayskie_figurki/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8&PAGEN_1=2'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, "html.parser")
-namae = namae + get_n(soup, 'figure', 'discomir')
-price = price + get_p(soup, 'figure', 'discomir')
-del namae[len(namae)-1]
-del price[len(namae)-1]
+namae = namae + get_n(soup, 'znachok', 'discomir')
+price = price + get_p(soup, 'znachok', 'discomir')
 for i in range(len(namae)):
     if price[i].isdigit()==True:
-        _df = pd.DataFrame([[namae[i], int(price[i]), 'Discomir', 'Figure']], columns=['name', 'price', 'shop', 'category'])
+        _df = pd.DataFrame([[namae[i], int(price[i]), 'Discomir', 'Pins']], columns=['name', 'price', 'shop', 'category'])
         df = df.append(_df, ignore_index=True)
 
-namae.clear()
-price.clear()
+# Old collect data
 
-# XL (the end 224)
-
-for k in range(2, 5):
-    url = 'https://xlm.ru/nendoroid?by=popular&page='+str(k)
-    namae = namae + get_n(url, 'figure', 'XL')
-    price = price + get_p(url, 'figure', 'XL')
-for i in range(len(namae)):
-    if price[i].isdigit() == True:
-        _df = pd.DataFrame([[namae[i], int(price[i]), 'XL Media', 'Figure']], columns=['name', 'price', 'shop', 'category'])
-        df = df.append(_df, ignore_index=True)
-
-namae.clear()
-price.clear()
-url = 'https://xlm.ru/manga?by=popular'
-namae = namae + get_n(url, 'manga', 'XL')
-price = price + get_p(url, 'manga', 'XL')
-for i in range(len(namae)):
-    if price[i].isdigit() == True:
-        _df = pd.DataFrame([[namae[i], int(price[i]), 'XL Media', 'Manga']], columns=['name', 'price', 'shop', 'category'])
-        df = df.append(_df, ignore_index=True)
-
-namae.clear()
-price.clear()
-
-for k in range(2, 19):
-    url = 'https://xlm.ru/manga?by=popular&page='+str(k)
-    namae = namae + get_n(url, 'manga', 'XL')
-    price = price + get_p(url, 'manga', 'XL')
-
-for i in range(len(namae)):
-    if price[i].isdigit() == True:
-        _df = pd.DataFrame([[namae[i], int(price[i]), 'XL Media', 'Manga']], columns=['name', 'price', 'shop', 'category'])
-        df = df.append(_df, ignore_index=True)
+# # Diskomir
+# url = 'https://diskomir.ru/catalog/manga/manga/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8'
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, "html.parser")
+# namae = namae + get_n(soup, 'manga', 'discomir')
+# price = price + get_p(soup, 'manga', 'discomir')
+# for k in range(2, 13):
+#     url = 'https://diskomir.ru/catalog/manga/manga/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8&PAGEN_1=' + str(k)
+#     page = requests.get(url)
+#     soup = BeautifulSoup(page.text, "html.parser")
+#     namae = namae + get_n(soup, 'manga', 'discomir')
+#     price = price + get_p(soup, 'manga', 'discomir')
+# del price[96]
+# del namae[96]
+#
+# df = pd.DataFrame(columns=['name', 'price', 'shop', 'category'])
+# for i in range(len(namae)):
+#     if price[i].isdigit() == True:
+#         _df = pd.DataFrame([[namae[i], int(price[i]), 'Discomir', 'Manga']], columns=['name', 'price', 'shop', 'category'])
+#         df = df.append(_df, ignore_index=True)
+# namae.clear()
+# price.clear()
+# url = 'https://diskomir.ru/catalog/animefigurki/malenkie_kitayskie_figurki/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8'
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, "html.parser")
+# namae = namae + get_n(soup, 'figure', 'discomir')
+# price = price + get_p(soup, 'figure', 'discomir')
+# url = 'https://diskomir.ru/catalog/animefigurki/malenkie_kitayskie_figurki/?arrFilter_ff%5BNAME%5D=&arrFilter_pf%5BNALICHIE%5D=86&arrFilter_pf%5BFROM_MOVIE%5D=&arrFilter_pf%5BMADE_BY%5D=&set_filter=%CD%E0%E9%F2%E8&PAGEN_1=2'
+# page = requests.get(url)
+# soup = BeautifulSoup(page.text, "html.parser")
+# namae = namae + get_n(soup, 'figure', 'discomir')
+# price = price + get_p(soup, 'figure', 'discomir')
+# del namae[len(namae)-1]
+# del price[len(namae)-1]
+# for i in range(len(namae)):
+#     if price[i].isdigit()==True:
+#         _df = pd.DataFrame([[namae[i], int(price[i]), 'Discomir', 'Figure']], columns=['name', 'price', 'shop', 'category'])
+#         df = df.append(_df, ignore_index=True)
+#
+# namae.clear()
+# price.clear()
+#
+# # XL (the end 224)
+#
+# for k in range(2, 5):
+#     url = 'https://xlm.ru/nendoroid?by=popular&page='+str(k)
+#     namae = namae + get_n(url, 'figure', 'XL')
+#     price = price + get_p(url, 'figure', 'XL')
+# for i in range(len(namae)):
+#     if price[i].isdigit() == True:
+#         _df = pd.DataFrame([[namae[i], int(price[i]), 'XL Media', 'Figure']], columns=['name', 'price', 'shop', 'category'])
+#         df = df.append(_df, ignore_index=True)
+#
+# namae.clear()
+# price.clear()
+# url = 'https://xlm.ru/manga?by=popular'
+# namae = namae + get_n(url, 'manga', 'XL')
+# price = price + get_p(url, 'manga', 'XL')
+# for i in range(len(namae)):
+#     if price[i].isdigit() == True:
+#         _df = pd.DataFrame([[namae[i], int(price[i]), 'XL Media', 'Manga']], columns=['name', 'price', 'shop', 'category'])
+#         df = df.append(_df, ignore_index=True)
+#
+# namae.clear()
+# price.clear()
+#
+# for k in range(2, 19):
+#     url = 'https://xlm.ru/manga?by=popular&page='+str(k)
+#     namae = namae + get_n(url, 'manga', 'XL')
+#     price = price + get_p(url, 'manga', 'XL')
+#
+# for i in range(len(namae)):
+#     if price[i].isdigit() == True:
+#         _df = pd.DataFrame([[namae[i], int(price[i]), 'XL Media', 'Manga']], columns=['name', 'price', 'shop', 'category'])
+#         df = df.append(_df, ignore_index=True)
 
 print(df)
 df.to_excel('output.xlsx')
--- main.py
+++ main.py
@@ -1,58 +1,16 @@
 import time
 from selenium import webdriver
-list=['a','b','c','d','e','f']
-numbers=[1,2,3]
-for n in sorted(numbers, reverse=True):
-    del list[n]
-print(list)
-# driver = webdriver.Firefox(executable_path=r'A:\geckodriver.exe')
-# driver.get("https://xlm.ru/search?search=%D0%A2%D0%BE%D0%BC+1&page=6")
-# time.sleep(6)
-#
-# but = driver.find_elements_by_class_name("costs-value")
-# a = []
-# for e in but:
-#     a.append(e.text)
-# print(a)
-# tag_list = driver.find_elements_by_class_name("product-name")
-# b = []
-# for e in tag_list:
-#     b.append(e.text)
-# print(len(b))
-# print(a)
-driver.quit()
-# url = 'https://xlm.ru/search?search=%D0%A2%D0%BE%D0%BC+1&page=6'
-# page = requests.get(url)
-# time.sleep(2)
-# soup = BeautifulSoup(page.text, "html.parser")
-# goods = soup.findAll('a', class_='product-name')
-# namae = []
-# pri = []
-# soup_p = soup.findAll('span', class_='costs-value')
-# print(soup_p)
-# for data in goods:
-#     namae.append(data.text[7:-5])
-# print(namae)
-# for data in soup_p:
-#     r = data
-#     if 'Цена' in r.text:
-#         dob = r.text[6:9]
-#         pri.append(dob)
-#     else:
-#         s = r.text[1:-3].find('₽')
-#         ik = r.text[s + 3:-3]
-#         dob = re.sub('\s', '', ik)
-#         pri.append(dob)
-# print(pri)
-# print(len(pri), len(namae))
-
-# for data in soup_p:
-#     namae.append(data)
-# s = namae[0].text[1:-3].find('₽')
-# ik = namae[0].text[s+3:-3]
-# dob = re.sub('\s', '', ik)
-# print(int(dob))
-
+from bs4 import BeautifulSoup
+import requests
+url = "https://diskomir.ru/catalog/znachki/nabory_znachkov/?from_movie=bananovaya_ryba_banana_fish_rybka_bananka&from_movie=voleybol_haikyu_haikyuu_&from_movie=doktor_stoun_dr_stone_doctor_stone&from_movie=drakon_gornichnaya_kobayashi_kobayashi_san_chi_no_maid_dragon_miss_kobayashi_s_dragon_maid&from_movie=durochka_aho_girl_ahogaru_clueless_girl"
+page = requests.get(url)
+soup = BeautifulSoup(page.text, "html.parser")
+goods = soup.findAll('a', class_='text_fader')
+for e in goods:
+    print(e.text[:-5])
+t_pri = soup.findAll('div', class_='price')
+for data in t_pri:
+    print(data.text[:-5])
 
 # import itertools
 # s = "ABC"
